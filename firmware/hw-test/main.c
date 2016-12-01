10c208f39d9595c9fb7190bc1d73e569ff17fc1e
Cfpintog <cfpintog@unal.edu.co>
2016-11-30 21:23:35 -0500

    Add files via upload

--- firmware/hw-test/main.c
+++ firmware/hw-test/main.c
@@ -13,15 +13,15 @@ char glob[] = "Global";
 #define SI  0x07	      //     0        1         1        1
 
 //Comunicación MIDI
-#define DO_3	   	0x30  //do  3ra octava
-#define RE_3	 	0x32  //re  3ra octava
-#define MI_3		0x34  //mi  3ra octava
-#define FA_3		0x35  //fa  3ra octava
-#define SOL_3		0x37  //sol 3ra octava
-#define LA_3		0x39  //la  3ra octava
-#define SI_3		0x3B  //si  3ra octava
+#define DO_3	   	0x24  //do  3ra octava
+#define RE_3	 	0x26  //re  3ra octava
+#define MI_3		0x28  //mi  3ra octava
+#define FA_3		0x29  //fa  3ra octava
+#define SOL_3		0x2B  //sol 3ra octava
+#define LA_3		0x2D  //la  3ra octava
+#define SI_3		0x2F //si  3ra octava
 #define NOTE_ON 	0x90  //nota on canal 1
-#define VEL_ON 		0x40  //velocidad de nota 64
+#define VEL_ON 		0x40 //velocidad de nota 64
 #define VEL_OFF		0x00  //apagado de nota
 #define CTRL_CH 	0xB0  //control change canal 1
 #define VOL_CH 		0x07  //cambio de volumen
@@ -34,8 +34,8 @@ extern uint32_t tic_msec;
 	uint8_t vel;	  //señal de velocidad de nota
 	uint8_t x;     //señal tono
 	uint8_t y;	//señal velocidad 
+	
 //	extern uint8_t v;
-
 int nota (int cmb); //Retorna la nota a partir de la combinación del guante
 int speed (int comb);	    //Retorna la velocidad a partir de la combinación		
 
@@ -46,14 +46,17 @@ int main()
 	{//uint8_t vol;     //señal de volumen transmitida en UART
 	uint8_t tn;
 	uint8_t vel;
+	uint8_t n_0;
+	
 	//uint8_t v;   	  //señal recibida de spi (conversor)
 	uint8_t n;   //señal recibida de gpio (fototransistor)
-	irq_set_mask(0x02);
-	irq_enable();
-		
-	tic_init();//funcion timer interrupcion
+	//irq_set_mask(0x02);
+	//irq_enable();
+	n_0=gpio_get();
+	//tic_init(); //funcion timer interrupcion
 		for(;;)
 		{//lectura de datos
+		
 		//conversor
 		spi_putchar0();
 		v=spi_getchar();
@@ -64,25 +67,40 @@ int main()
 		
 		//adecuacion señales tono y velocidad		
 		tn=nota(n);
+		
 		vel=speed(n);
-	
+		
 		
 		//Transmisión de mensaje MIDI
-		//Volumen
-		//uart_putchar(CTRL_CH);//0xB0
-		//uart_putchar(VOL_CH);//0x07
-		//uart_putchar(v);
-		msleep(10);
-		//Tipo de nota
-		
+
+
+			if(n_0==n){
+					}
+	
+	else
+		{n_0=n;
 		uart_putchar(NOTE_ON);//0x90
 		uart_putchar(tn);//
 		uart_putchar(vel);
+	}
+	
+		//Tipo de nota
+		
+
+		//Volumen
+		uart_putchar(CTRL_CH);//0xB0
+		uart_putchar(VOL_CH);//0x07
+		uart_putchar(v);
+				
 		msleep(100);
 		
 		}
 	}
 
+
+	
+
+		
 
 
 	int nota (int cmb)
